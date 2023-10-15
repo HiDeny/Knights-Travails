ed4355095d9568499edf1397163c645155783239
HiDeny <114344945+HiDeny@users.noreply.github.com>
2023-10-15 17:52:34 +0200

    Final update

--- kinghtMoves.js
+++ kinghtMoves.js
@@ -1,4 +1,7 @@
 //* knightMoves - shows the shortest possible way to get from one square to another by outputting all squares the knight will stop on along the way.
+//TODO: Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board.
+//TODO: Decide which search algorithm is best to use for this case. Hint: one of them could be a potentially infinite series.
+//TODO: Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square.
 
 class Knight {
   constructor(position) {
@@ -21,17 +24,19 @@ class Knight {
   moveTo(target) {
     const targetX = target[0];
     const targetY = target[1];
+
+    if (this.position[0] === targetX && this.position[1] === targetY) {
+      console.log('You are on this position!');
+      return 'You are at this position!';
+    }
     if (targetX > 7 || targetX < 0 || targetY > 7 || targetY < 0) {
       console.error('Target out of board!');
       return 'Target out of board!';
     }
 
     let queue = [this.position];
-    const visited = new Set();
-    // const path = new Map();
+    const visited = new Set(`${this.position[0]},${this.position[1]}`);
     const path = {};
-
-    visited.add(this.position);
 
     while (queue.length) {
       let currentMove = queue.shift();
@@ -40,73 +45,54 @@ class Knight {
       let nextPossibleMoves = this.possibleMoves(currentMove);
 
       if (currentX === targetX && currentY === targetY) {
-        console.log(path);
         const shortestPath = [];
-        let step = target;
-        console.log(path[`${step[0]},${step[1]}`]);
+        const basePositionStr = `${this.position[0]},${this.position[1]}`;
+        let stepStr = `${targetX},${targetY}`;
 
-        while (step && step !== `${this.position[0]},${this.position[1]}`) {
-          console.log(step);
-          shortestPath.push(`${step}`);
-          step = path[step];
+        while (stepStr && stepStr !== basePositionStr) {
+          shortestPath.push(stepStr);
+          stepStr = path[stepStr];
         }
-        shortestPath.push(`${this.position}`);
-        console.log(shortestPath);
-        queue = [];
-        console.log('Found It!');
-        return 'Found it!';
+        shortestPath.push(basePositionStr);
+
+        const endingWord = shortestPath.length - 1 > 1 ? 'moves' : 'move';
+        console.log(`You made it in ${shortestPath.length - 1} ${endingWord}!`);
+
+        let shortestPathStr = '';
+        while (shortestPath.length) {
+          const currentPath = shortestPath.pop();
+          shortestPathStr +=
+            shortestPath.length >= 1 ? `${currentPath} -> ` : `${currentPath}`;
+        }
+
+        console.log(shortestPathStr);
+        return shortestPathStr;
       }
 
       for (let move in nextPossibleMoves) {
-        let nextMove = nextPossibleMoves[move];
+        const nextMove = nextPossibleMoves[move];
+        const nextX = nextMove[0];
+        const nextY = nextMove[1];
+        const nextMoveStr = `${nextX},${nextY}`;
 
-        let nextX = nextMove[0];
-        let nextY = nextMove[1];
         if (
           nextX < 8 &&
           nextX >= 0 &&
           nextY < 8 &&
           nextY >= 0 &&
-          !visited.has(`${nextX}, ${nextY}`)
+          !visited.has(nextMoveStr)
         ) {
           queue.push(nextMove);
-          visited.add(`${nextX}, ${nextY}`);
-          path[nextMove] = `${currentX},${currentY}`;
+          visited.add(nextMoveStr);
+          path[nextMoveStr] = `${currentX},${currentY}`;
         }
       }
     }
   }
 }
 
-//TODO: Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board.
-
-//TODO: Decide which search algorithm is best to use for this case. Hint: one of them could be a potentially infinite series.
-
-//TODO: Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square.
-
 const testKnight = new Knight([0, 0]);
 
 testKnight.moveTo([5, 5]);
 testKnight.moveTo([3, 6]);
 testKnight.moveTo([1, 2]);
-
-// const testMap = new Map();
-// const test1 = [0, 0];
-// const test2 = [1, 4];
-// const test3 = [3, 3];
-// const test4 = [4, 2];
-// testMap.set(test2, test1);
-// testMap.set(test3, test2);
-// testMap.set(test4, test3);
-
-// let stepTest = test4;
-// let result = [];
-// while (stepTest) {
-//   result.push(stepTest);
-//   stepTest = testMap.get(stepTest);
-//   console.log(stepTest);
-// }
-
-// const nextStep = testMap.get(stepTest);
-
-// console.log(result);
